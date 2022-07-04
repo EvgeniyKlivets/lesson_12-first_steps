efb76ccf73b393092d84104a5834433098055700
Evgeniy KLivets <98554759+EvgeniyKlivets@users.noreply.github.com>
2022-07-04 23:47:37 +0300

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -5,15 +5,19 @@ require_once BASE_DIR. '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv:: createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 
+if (!session_id()) {
+    session_start();
+}
+
 try{
     $router = new \Core\Router();
 
-    require_once  BASE_DIR. '/routers/web.php';
+    require_once  BASE_DIR . '/routers/web.php';
 
     if (!preg_match('/assets/i', $_SERVER['REQUEST_URI'])){
         $router->dispatch($_SERVER['REQUEST_URI']);
     }
 
 } catch(Exception $e){
-    /*dd($e->getMessage());*/
+    dd($e->getMessage());
 }
